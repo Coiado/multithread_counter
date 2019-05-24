316a2c0960a54fcecfb2daadf2033138d0817a91
Lucas Coiado Mota <lucascoiado55@gmail.com>
2019-05-23 23:35:15 -0300

    Terminando tarefa de multithread

--- src/main.c
+++ src/main.c
@@ -6,12 +6,82 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <string.h>
+
+#define TAMANHO 1000
+
+#define WORKERS 4
+
+
+int resposta = 0;
+pthread_mutex_t trava;
+int numeros[TAMANHO];
+int pos = 0;
+
+int primo(int num)
+{
+    if (num == 1 || num == 0 ) return 0;
+    if (num == 2) return 1;
+    if (num % 2 == 0 && num > 2) return 0;
+    for(int i = 3; i < num; i+= 1)
+    {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+void* worker(void *arg) {
+    int *N = (int*)(arg);
+    int M = (*N);
+    while (1) {
+        
+        pthread_mutex_lock(&trava);
+        if (pos >= M) break;
+        int valor = numeros[pos];
+        pos++;
+        pthread_mutex_unlock(&trava);
+        resposta += primo(valor);
+    }
+    return NULL;
+}
+
+
+
 
 int main() {
-
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+    pthread_t workers[WORKERS];
+    char text[TAMANHO];
+    
+    /* Pegando input */
+    scanf("%[^\n]",text);
+    
+    char *token = strtok(text, " ");
+    
+    int tam = 0;
+    
+    while(token != NULL)
+    {
+        numeros[tam] = atoi(token);
+        token = strtok(NULL, " ");
+        tam++;
+        
+    }
+    /* Disparanto threads */
+    for (int i=0; i<WORKERS; i++) {
+        pthread_create(&(workers[i]), NULL, worker, (void*) (&tam));
+    }
+    
+    /* Esperando threads */
+    for (int i=0; i<WORKERS; i++) {
+        pthread_join(workers[i], NULL);
+    }
+    
+    /* Escrevendo resposta */
+    printf("%d\n", resposta);
+    
+    return 0;
 }
